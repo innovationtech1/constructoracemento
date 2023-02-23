8f65fbf7c5fe915218778749db3053bdae85a24b
Hector De Hoyos <hectordehoyos053@gmail.com>
2023-02-23 14:21:41 -0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+
+
+import './App.css'
+import CreandoProyecto from './components/CreandoProyecto'
+function App() {
+
+
+  return (
+    <div className="App">
+      <CreandoProyecto/>
+   
+   
+    </div>
+
+    
+  )
+}
+
+export default App
---
+++ src/components/CreandoProyecto.jsx
@@ -0,0 +1,14 @@
+import logo from "../assets/logo.png"
+import "../App.css";
+function CreandoProyecto() {
+  return (
+    <div>
+      <div className="tilte">Iniciando Proyecto</div>
+
+      <h1 className="title2">Configurando sitio web</h1>
+      <img className="animacionLogo" src={logo} alt="logo" />
+    </div>
+  );
+}
+
+export default CreandoProyecto;
